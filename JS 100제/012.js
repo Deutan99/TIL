ecb0261f85ea08d3e2c912974671b60f32388c22
HEO JAE HYEOK <41223338+Deutan99@users.noreply.github.com>
2023-03-15 23:39:06 +0900

    JS 100제 012번

---
+++ JS 100제/012.js
@@ -0,0 +1,15 @@
+class Wizard {
+    constructor(health, mana, armor) {
+      this.health = health;
+      this.mana = mana;
+      this.armor = armor;
+    }
+    attack() {
+      console.log("파이어볼");
+    }
+} // const Wizard = class Wizard {}로 선언해도 무방하다.
+  
+const x = new Wizard(545, 210, 10);
+console.log(x.health, x.mana, x.armor);
+x.attack();
+  
