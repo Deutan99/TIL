3dc189314e7c5c47758548a04c9483cb442c08f5
HEO JAE HYEOK <41223338+Deutan99@users.noreply.github.com>
2024-03-05 03:38:04 +0900

    중복 제거하기

---
+++ SQL/sol26.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT COUNT(DISTINCT NAME) AS count
+FROM ANIMAL_INS;
+
+-- # #1
+-- # SELECT COUNT(DISTINCT name) FROM [table]
+-- # #2
+-- # SELECT COUNT(DISTINCT(name)) FROM [table]
